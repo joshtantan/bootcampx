9979af7f72b613f47c39a184e4c6e6e23d8368d0
Joshua Tan <josh.tan.tann@gmail.com>
2020-10-14 23:03:05 -0700

    Protect against SQL injections by using parameter querying
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,19 @@
 const { Pool } = require('pg');
 
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+const queryString = `
+  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests.*) AS total_assistances 
+  FROM assistance_requests
+  JOIN students ON student_id = students.id
+  JOIN teachers ON teacher_id = teachers.id
+  JOIN cohorts ON students.cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  GROUP BY teachers.name, cohorts.name
+  ORDER BY teachers.name;
+`;
+
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -7,16 +21,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests.*) AS total_assistances 
-  FROM assistance_requests
-  JOIN students ON student_id = students.id
-  JOIN teachers ON teacher_id = teachers.id
-  JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-  GROUP BY teachers.name, cohorts.name
-  ORDER BY teachers.name;
-  `)
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(report => {
       console.log(`${report.cohort}: ${report.teacher}`);
